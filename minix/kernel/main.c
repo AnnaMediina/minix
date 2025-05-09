70faefe048171d39465d2d0dfd7f0499ab7e5348
Anna Clara Medina Roissmann <138173817+AnnaMediina@users.noreply.github.com>
2025-05-09 15:02:34 -0300

    Update main.c

--- minix/kernel/main.c
+++ minix/kernel/main.c
@@ -344,10 +344,10 @@ static void announce(void)
       OS_RELEASE);
   printf("MINIX is open source software, see http://www.minix3.org\n");
 		/*print adicionado com a mensagem*/ 
-  printf("========================================")
-  printf("| Minix 3.4.Orc6 - SO - UNIFESP 1s2025 |")
-  printf("|    Projeto Pratico (I) - Equipe 2    |")
-  printf("========================================")
+  printf("========================================");
+  printf("| Minix 3.4.Orc6 - SO - UNIFESP 1s2025 |");
+  printf("|    Projeto Pratico (I) - Equipe 2    |");
+  printf("========================================");
 }
 
 /*===========================================================================*
@@ -364,10 +364,10 @@ void prepare_shutdown(const int how)
    */
   printf("MINIX will now be shut down ...\n");
 	/*print da mensagem adicionado*/
-  printf("=========================================\n")
-  printf("| ATE A PROXIMA - SO - UNIFESP - 1S2025 |\n")
-  printf("|     Projeto Pratico (I) - Equipe 2    |\n")
-  printf("=========================================\n")
+  printf("=========================================\n");
+  printf("| ATE A PROXIMA - SO - UNIFESP - 1S2025 |\n");
+  printf("|     Projeto Pratico (I) - Equipe 2    |\n");
+  printf("=========================================\n");
   set_kernel_timer(&shutdown_timer, get_monotonic() + system_hz,
       minix_shutdown, how);
 }
